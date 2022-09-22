965f47f639eaf4b83d155ea65c043c0166d24c3f
doppelvincent <vvincent891@gmail.com>
2022-09-22 20:47:36 +0200

    first commit to github

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+
+  TransactionList(this.transactions, this.deleteTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constrains) {
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'No transactions added yet!',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                      height: constrains.maxHeight * 0.6,
+                      child: Image.asset('assets/images/waiting.png',
+                          fit: BoxFit.cover)),
+                ],
+              ),
+            );
+          })
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: FittedBox(
+                        child: Text("${transactions[index].amount}€"),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => deleteTx(transactions[index].id),
+                    color: Colors.red,
+                  ),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
+  }
+}
